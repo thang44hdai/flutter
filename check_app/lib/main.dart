4cd5c6e921b5a4658128cc3ec08beea2f4ba5bbc
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-08-23 17:57:12 +0700

    .

--- check_app/lib/main.dart
+++ check_app/lib/main.dart
@@ -1,20 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  //runApp(const MainApp(,));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Youtube"),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+            child: widget2(false),
+          ),
+        ),
+      ),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class widget1 extends StatelessWidget {
+  final bool loading;
+  widget1(this.loading);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('xin chafo ashfhfhes'),
-        ),
-      ),
-    );
+    if (loading) {
+      return CircularProgressIndicator();
+    } else {
+      return Text("data");
+    }
   }
 }
+
+class widget2 extends StatefulWidget {
+  bool loading;
+  widget2(this.loading);
+  @override
+  State<StatefulWidget> createState() {
+    return state_widget2();
+  }
+}
+
+class state_widget2 extends State<widget2> {
+  @override
+  void initState() {
+    widget.loading = true;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return widget.loading
+        ? const CircularProgressIndicator()
+        : const Text("data");
+  }
+}
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('xin'),
+//         ),
+//       ),
+//     );
+//   }
+// }
