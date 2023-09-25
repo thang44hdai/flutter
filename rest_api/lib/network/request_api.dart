0833491bc36cb6faf665cbcf175297af7b4e7f96
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-09-25 11:15:31 +0700

    .

--- rest_api/lib/network/request_api.dart
+++ rest_api/lib/network/request_api.dart
@@ -5,16 +5,18 @@ import 'package:http/http.dart' as http;
 
 class networkRequest {
   static const String url = "https://jsonplaceholder.typicode.com/posts";
-  static List<Post> parsePost(String responseBody) {
-    var list = json.decoder(responseBody) as List<dynamic>;
-    List<Post> posts = list.map((e) => Post.fromJson(e)).toList();
-    return posts;
-  }
-
-  static Future<List<Post>> fetchPosts({int page = 1}) async {
-    final response = await http.get(url as Uri);
+  // static List<Post> parsePost(List<dynamic> list) {
+  //   List<Post> posts = list.map((e) => Post.fromJson(e)).toList();
+  //   return posts;
+  // }
+  Future<List<Post>> fetchPosts(Function(List<Post>) onDataFetched) async {
+    final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      return compute(parsePost, response.body);
+      final List<dynamic> jsonData = json.decode(response.body);
+      final List<Post> posts =
+          jsonData.map((data) => Post.fromJson(data)).toList();
+      onDataFetched(posts);
+      return posts;
     } else if (response.statusCode == 404) {
       throw Exception('Not Found');
     } else {
--- rest_api/lib/screen.dart
+++ rest_api/lib/screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rest_api/models/post.dart';
+import 'package:rest_api/network/request_api.dart';
 
 class screen extends StatefulWidget {
   const screen({super.key});
@@ -8,14 +10,47 @@ class screen extends StatefulWidget {
 }
 
 class _screenState extends State<screen> {
+  List<Post> postData = [];
+  final data = networkRequest();
+  @override
+  void initState() {
+    super.initState();
+    fetchDataFromNetwork();
+  }
+
+  void fetchDataFromNetwork() {
+    data.fetchPosts((data) {
+      setState(() {
+        postData = data; // Cập nhật danh sách bài viết
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("xin chao"),
       ),
-      body: Center(
-        child: Text("Chao tam biet cac ban nhe"),
+      body: Container(
+        child: ListView.builder(
+          itemCount: postData.length,
+          itemBuilder: (BuildContext context, int index) {
+            final data = postData[index];
+            return Card(
+              child: ListTile(
+                title: Column(
+                  children: [
+                    //Text("data")
+                    Text(data.userId.toString()),
+                    Text(data.title.toString()),
+                    Text(data.body.toString()),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
