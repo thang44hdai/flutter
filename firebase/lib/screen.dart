ecbefbde699ce90a58c94971cc94519cf8c643e4
unknown <thangquang8888@gmail.com>
2023-10-02 17:55:11 +0700

    .

--- firebase/lib/screen.dart
+++ firebase/lib/screen.dart
@@ -1,70 +1,39 @@
-import 'package:firebase/user.dart';
 import 'package:flutter/material.dart';
 
-class screen extends StatefulWidget {
-  const screen({super.key});
-
+class SourceScreen extends StatelessWidget {
   @override
-  State<screen> createState() => _screenState();
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DestinationScreen(),
+          ),
+        );
+      },
+      child: Hero(
+        tag: 'imageHero',
+        child: Image.asset('path_to_image'),
+      ),
+    );
+  }
 }
 
-class _screenState extends State<screen> {
-  TextEditingController _controllerName = TextEditingController();
-  TextEditingController _controllerDate = TextEditingController();
-  List<user> list_user = [];
-  @override
-  void initState() {
-    list_user.add(user("Nguyen Quang Thang", "04/04/2003"));
-    super.initState();
-  }
-
+class DestinationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("List User"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: list_user.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    leading: Icon(Icons.star),
-                    title: Column(
-                      children: [
-                        Text(list_user[index].name),
-                        Text(list_user[index].date),
-                      ],
-                    ),
-                    trailing: Icon(Icons.ac_unit),
-                  ),
-                );
-              },
-            ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.asset('path_to_image'),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            controller: _controllerName,
-            decoration: InputDecoration(hintText: "Nhập họ và tên:"),
-          ),
-          TextField(
-            controller: _controllerDate,
-            decoration: InputDecoration(hintText: "Nhập ngày tháng năm sinh"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                list_user.add(user(_controllerName.text, _controllerDate.text));
-              });
-            },
-            child: Text("Add User"),
-          )
-        ],
+        ),
       ),
     );
   }
